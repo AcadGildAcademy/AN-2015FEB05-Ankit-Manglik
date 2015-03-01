ff95542f77eabcf41a6dd1dc81eef5e0241cd68d
ankitmanglik <ankit.manglik@gmail.com>
2015-03-01 18:53:19 +0530

    UI Fundamentals Assignment

---
+++ UIFundamentals/app/src/main/java/com/afewsomethings/uifundamentals/MainMenu.java
@@ -0,0 +1,40 @@
+package com.afewsomethings.uifundamentals;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by ankit on 01-03-2015.
+ */
+public class MainMenu extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.rainbow);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
